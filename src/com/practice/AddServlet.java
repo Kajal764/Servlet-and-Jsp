58fa099f209db01e67f36e5297bad4093f8a6e14
kajal764 <kajaldw666@gmail.com>
2020-08-26 16:41:14 +0530

    Add: Square servlet and call one servlet from another

--- src/com/practice/AddServlet.java
+++ src/com/practice/AddServlet.java
@@ -3,20 +3,24 @@ package com.practice;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
-	
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
-		 int a=Integer.parseInt(req.getParameter("num1"));
-		 int b= Integer.parseInt(req.getParameter("num2"));
-		 
-		 int sum=a+b;
-		 
-		  PrintWriter out=res.getWriter();
-		  out.println("result is "+sum);
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		int a = Integer.parseInt(req.getParameter("num1"));
+		int b = Integer.parseInt(req.getParameter("num2"));
+
+		int sum = a + b;
+
+		req.setAttribute("sum", sum);
+
+		RequestDispatcher rd = req.getRequestDispatcher("sq");
+		rd.forward(req, res);
 	}
 
 }
---
+++ src/com/practice/SqServlet.java
@@ -0,0 +1,20 @@
+package com.practice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SqServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+		int result = (int) req.getAttribute("sum");
+		int square = result * result;
+		PrintWriter out = res.getWriter();
+
+		out.print("square result " + square);
+	}
+}
