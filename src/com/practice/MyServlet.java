747d6b4be4e39a86789d232e255bdfc330e4caf9
kajal764 <kajaldw666@gmail.com>
2020-08-27 12:23:16 +0530

    Add: Servlet context to get byDefault data

---
+++ src/com/practice/MyServlet.java
@@ -0,0 +1,24 @@
+package com.practice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		PrintWriter out = response.getWriter();
+		out.print("Hiii ");
+
+		ServletContext ctx= getServletContext();
+		String str= ctx.getInitParameter("name");
+		out.print(str);
+		
+
+	}
+
+}
